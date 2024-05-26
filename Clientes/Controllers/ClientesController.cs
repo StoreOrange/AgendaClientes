8af40fa987482da007451a4a9d2aee651d859877
StoreOrange <oddgarcia.samuel@gmail.com>
2024-05-25 23:33:08 -0600

    Mensaje del commit

--- Clientes/Controllers/ClientesController.cs
+++ Clientes/Controllers/ClientesController.cs
@@ -22,6 +22,107 @@ namespace Clientes.Controllers
             return View(_context.Clientes.ToList());
         }
 
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+  [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear([Bind("Nombres,Apellidos,Correo,Dirección,Compañía,Nota,FechaDeRegistro")] Clientes.Models.Clientes cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        public async Task<IActionResult> Editar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return View(cliente);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(int id, [Bind("ID,Nombres,Apellidos,Correo,Dirección,Compañía,Nota,FechaDeRegistro")] Clientes.Models.Clientes cliente)
+        {
+            if (id != cliente.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cliente);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClienteExists(cliente.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        public async Task<IActionResult> Eliminar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes.FirstOrDefaultAsync(m => m.ID == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        [HttpPost, ActionName("Eliminar")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ConfirmarEliminar(int id)
+{
+    var cliente = await _context.Clientes.FindAsync(id);
+    if (cliente == null)
+    {
+        return NotFound();
     }
 
+    _context.Clientes.Remove(cliente);
+    await _context.SaveChangesAsync();
+    return RedirectToAction(nameof(Index));
+}
+
+
+        private bool ClienteExists(int id)
+        {
+            return _context.Clientes.Any(e => e.ID == id);
+        }
+    }
 }
